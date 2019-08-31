a49bd516fdb91bd19f13ae13be7c6f6224fa39fb
Gaurav Kumar <gkumar@techfak.uni-bielefeld.de>
2019-08-31 16:01:23 +0200

    kl div

--- baseline/common/models.py
+++ baseline/common/models.py
@@ -102,6 +102,36 @@ def mlp(num_layers=3, num_hidden=64, activation=tf.nn.relu, layer_norm=False):
 
     return network_fn
 
+@register("encoder_mlp")
+def encoder_mlp(num_layers=1, num_hidden=64, activation=tf.nn.relu, layer_norm=False):
+    """
+    Stack of fully-connected layers to be used as a variational encoder
+
+    Parameters:
+    ----------
+
+    num_layers: int                 number of fully-connected layers (default: 2)
+
+    num_hidden: int                 size of fully-connected layers (default: 64)
+
+    activation:                     activation function (default: tf.tanh)
+
+    Returns:
+    -------
+
+    function that builds fully connected network with a given input tensor / placeholder
+    """
+    def network_fn(X):
+        h = tf.layers.flatten(X)
+        for i in range(num_layers):
+            h = fc(h, 'encoder_mlp_fc{}'.format(i), nh=num_hidden, init_scale=np.sqrt(2))
+            if layer_norm:
+                h = tf.contrib.layers.layer_norm(h, center=True, scale=True)
+            h = activation(h)
+
+        return h
+
+    return network_fn
 
 
 @register("cnn")
