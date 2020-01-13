7791c7df2b2e30afd1e3703ae4cebadec2660eb0
gkumar <gaurav.kumar@techfak.uni-bielefeld.de>
2020-01-13 12:13:46 +0100

    new models

---
+++ mazeContinuousTarget_fixed_camera/Assets/scripts/bottomCollider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bottomCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	public bool state;
+	void OnCollisionEnter(Collision col)
+	{
+		state = true;
+	}
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ mazeContinuousTarget_fixed_camera/Assets/scripts/colider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+	public bool stat;
+	void OnCollisionEnter(Collision col)
+	{
+		stat = true;
+
+	}
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
