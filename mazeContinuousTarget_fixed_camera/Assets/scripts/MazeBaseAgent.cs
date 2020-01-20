30c4b2e55121da2923c71bd6fab81b0a851f96fb
Gaurav Kumar <gkumar@techfak.uni-bielefeld.de>
2020-01-21 00:53:04 +0100

    transfer

--- mazeContinuousTarget_fixed_camera/Assets/scripts/MazeBaseAgent.cs
+++ mazeContinuousTarget_fixed_camera/Assets/scripts/MazeBaseAgent.cs
@@ -13,7 +13,11 @@ public class MazeBaseAgent : Agent
 
 	public int[,] Maze;
 
-	public float y_pos = 0.6f;
+	public float y_pos_ball = 0.75f;
+	public float y_pos_goal = 0.51f;
+
+	public float delta_rot_x;
+	public float delta_rot_z;
 
 	private GameObject wall;
 	private GameObject temp; 
@@ -158,9 +162,6 @@ public class MazeBaseAgent : Agent
 
 	public override void AgentReset(){
 		
-		gameObject.transform.position = new Vector3 (0, 0, 0);
-		gameObject.transform.rotation = new Quaternion (0.1f, 0, 0.1f, 1);
-
 		//Ball.transform.position = new Vector3 (0.0f, 0.55f, 0.0f);
 		//Target.position = new Vector3 (0.0f, 0.05f, 0.0f);
 
@@ -176,7 +177,7 @@ public class MazeBaseAgent : Agent
 			ball_y = UnityEngine.Random.Range(1, 67);
 		}
 
-		Ball.transform.position = new Vector3 (-33.0f + ball_x, y_pos, -33.0f + ball_y);
+		Ball.transform.position = new Vector3 (-33.0f + ball_x, y_pos_ball, -33.0f + ball_y);
 
 		// Random ball position in feasible space
 		ball_x = UnityEngine.Random.Range(1, 67);
@@ -187,17 +188,15 @@ public class MazeBaseAgent : Agent
 			ball_y = UnityEngine.Random.Range(1, 67);
 		}
 
-		Goal.transform.position = new Vector3 (-33.0f + ball_x, y_pos, -33.0f + ball_y);
+		Goal.transform.position = new Vector3 (-33.0f + ball_x, y_pos_goal, -33.0f + ball_y);
+		Goal.transform.parent = gameObject.transform;
 
 		Ball.GetComponent<Rigidbody>().AddForce(0, 0, 0);
 		Ball.GetComponent<Rigidbody>().velocity = new Vector3(0.0f, 0.0f, 0.0f);
 
+		gameObject.transform.position = new Vector3 (0, 0, 0);
+		gameObject.transform.rotation = new Quaternion (0.0f, 0.0f, 0.0f, 1);
 
-		Debug.Log (Ball.transform.position);
-		Debug.Log (Goal.transform.position);
-
-
-		//CollectObservations ();
 	}
 
 
@@ -225,17 +224,41 @@ public class MazeBaseAgent : Agent
 	}
 
 
+	void OnGUI() {
+		GUI.Label (new Rect (200, 8, 240, 160), "Angular Rotation: " + gameObject.transform.eulerAngles);
+		GUI.Label (new Rect (200, 40, 240, 160), "Angular Velocity: " + gameObject.GetComponent<Rigidbody>().angularVelocity);
+		GUI.Label (new Rect (200, 72, 240, 160), "Delta Rotation: " + delta_rot_x +" "+ delta_rot_z);
+	}
+
+	/*
+
 	public override void AgentAction(float[] vectorAction, string textAction){
 
 		//Vector3 BallTransform = Ball.transform.position;
 		//Ball.GetComponent<Rigidbody>().AddForce(vectorAction[2], 0, vectorAction[3]);
 		//Ball.GetComponent<Rigidbody>().velocity = new Vector3(vectorAction[2], 0.0f, vectorAction[3]);
 
+		//Debug.Log (actionZ);
+		//Debug.Log (actionX);
+
+
+		// desired rotation
 		var actionZ = vectorAction[0];
 		var actionX = vectorAction[1];
 
-		//Debug.Log (actionZ);
-		//Debug.Log (actionX);
+
+		// current rotation
+		float current_rot_x = gameObject.transform.rotation.x;
+		float current_rot_z = gameObject.transform.rotation.z;
+
+
+		// delta rotation
+		delta_rot_x = actionX - current_rot_x;
+		delta_rot_z = actionZ - current_rot_z;
+
+
+
+		gameObject.GetComponent<Rigidbody>().angularVelocity = new Vector3(delta_rot_x/10, 0.0f, delta_rot_z/10);
 
 		//var actionZ = 2f * Mathf.Clamp(vectorAction[0], -1f, 1f);
 		//var actionX = 2f * Mathf.Clamp(vectorAction[1], -1f, 1f);
@@ -243,6 +266,7 @@ public class MazeBaseAgent : Agent
 		if ((gameObject.transform.rotation.x < 0.25f && actionX > 0f) ||
 			(gameObject.transform.rotation.x > -0.25f && actionX < 0f)) {
 
+			//gameObject.GetComponent<Rigidbody>().angularVelocity = new Vector3(delta_rot_x/10, 0.0f, 0.0f);
 			gameObject.transform.Rotate(new Vector3(1, 0, 0), actionX);
 		}
 
@@ -254,7 +278,6 @@ public class MazeBaseAgent : Agent
 
 		//Debug.Log (gameObject.transform.rotation);
 
-		/*
 		// moving ball to next position
 		int x_pos = (int)(-36.0f + vectorAction[0]); 
 		int y_pos = (int)(36.0f - vectorAction[1]);
@@ -267,40 +290,90 @@ public class MazeBaseAgent : Agent
 		if (Maze [(int)vectorAction[0], (int)vectorAction[1]] == 1) {
 			Ball.transform.position = new Vector3 (x_pos, 0.55f, y_pos);
 			SetReward (0.1f);
-		}*/
 	}
+	*/
 
-	/*
 	public override void AgentAction(float[] vectorAction, string textAction){
-
-		Debug.Log (vectorAction[0]);
-		Debug.Log (vectorAction[1]);
 
 		Vector3 BallTransform = Ball.transform.position;
 
-		var actionZ = 2f * Mathf.Clamp(vectorAction[0], -1f, 1f);
-		var actionX = 2f * Mathf.Clamp(vectorAction[1], -1f, 1f);
+		//var actionZ = 2f * Mathf.Clamp(vectorAction[0], -1f, 1f);
+		//var actionX = 2f * Mathf.Clamp(vectorAction[1], -1f, 1f);
 
-		if ((gameObject.transform.rotation.z < 0.25f && actionZ > 0f) ||
-			(gameObject.transform.rotation.z > -0.25f && actionZ < 0f)) {
+		// desired rotation
+		var actionZ = vectorAction[0];
+		var actionX = vectorAction[1];
+
+		// current rotation
+		float current_rot_x = gameObject.transform.eulerAngles.x;
+		float current_rot_z = gameObject.transform.eulerAngles.z;
+
+		// delta rotation
+		delta_rot_x = actionX - current_rot_x;
+		delta_rot_z = actionZ - current_rot_z;
+
+
+		//gameObject.GetComponent<Rigidbody>().angularVelocity = new Vector3((float)delta_rot_x/10, 0.0f, (float)delta_rot_z/10);
+
+
+		if ((-5.25f < gameObject.transform.eulerAngles.z && 
+			gameObject.transform.eulerAngles.z < 5.25f)) {
 
 			gameObject.transform.Rotate(new Vector3(0, 0, 1), actionZ);
+
+		} else if ((-5.25f > gameObject.transform.eulerAngles.z && actionZ > 0.0f) ||
+			(5.25f < gameObject.transform.eulerAngles.z && actionZ < 0.0f)) {
+
+			gameObject.transform.Rotate(new Vector3(0, 0, 1), actionZ);
+		
 		}
 
-		if ((gameObject.transform.rotation.x < 0.25f && actionX > 0f) ||
-			(gameObject.transform.rotation.x > -0.25f && actionX < 0f)) {
+
+		if ((-5.25f < gameObject.transform.eulerAngles.x && 
+			gameObject.transform.eulerAngles.x < 5.25f)) {
 
 			gameObject.transform.Rotate(new Vector3(1, 0, 0), actionX);
+
+		} else if ((-5.25f > gameObject.transform.eulerAngles.x && actionX > 0.0f) ||
+			(5.25f < gameObject.transform.eulerAngles.x && actionX < 0.0f)) {
+
+			gameObject.transform.Rotate(new Vector3(1, 0, 0), actionX);
+
 		}
 
-		float distanceToTarget = Vector3.Distance (Ball.transform.position, Target.position);
+
+		float distanceToTarget = Vector3.Distance (Ball.transform.position, Goal.transform.position);
 
 		if (distanceToTarget < 0.5f) {
 			SetReward (0.0f);
 			Done ();
-		} else if (BallTransform.y - gameObject.transform.position.y < -2f ||
-			Mathf.Abs (BallTransform.x - gameObject.transform.position.x) > 2.5f ||
-			Mathf.Abs (BallTransform.z - gameObject.transform.position.z) > 2.5f) {
+		} else if (BallTransform.y - gameObject.transform.position.y < -20f ||
+			Mathf.Abs (BallTransform.x - gameObject.transform.position.x) > 33.5f ||
+			Mathf.Abs (BallTransform.z - gameObject.transform.position.z) > 33.5f) {
+			SetReward (-11.0f);
+			Done ();
+		}
+
+
+
+		/*
+
+		if ((0.0f < gameObject.transform.rotation.x
+			&& gameObject.transform.rotation.x < 5.0f 
+			&& actionX > 0.0f) ||
+			(0.0f > gameObject.transform.rotation.x
+				&& gameObject.transform.rotation.x > -5.0f 
+				&& actionX < 0.0f)) {
+
+			gameObject.transform.Rotate(new Vector3(1, 0, 0), actionX);
+		}
+
+		if (distanceToTarget < 0.5f) {
+			SetReward (0.0f);
+			Done ();
+		} else if (BallTransform.y - gameObject.transform.position.y < -20f ||
+			Mathf.Abs (BallTransform.x - gameObject.transform.position.x) > 33.5f ||
+			Mathf.Abs (BallTransform.z - gameObject.transform.position.z) > 33.5f) {
 			SetReward (-11.0f);
 			Done ();
 		} else {
@@ -319,6 +392,9 @@ public class MazeBaseAgent : Agent
 				SetReward (-10.0f);
 			}
 		}
-	}*/
 
+
+		*/
+
+	}
 }
