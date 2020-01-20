fc8e6939e044bb419e1361d511b83de62ad25212
gkumar <gaurav.kumar@techfak.uni-bielefeld.de>
2020-01-20 11:30:41 +0100

    environment for a start planner

---
+++ mazeDiscreteTarget_add_force/Assets/scripts/GameAcademy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+
+public class GameAcademy : Academy
+{
+    public override void AcademyReset()
+    {
+        
+    }
+
+    public override void AcademyStep()
+    {
+        
+    }
+}
